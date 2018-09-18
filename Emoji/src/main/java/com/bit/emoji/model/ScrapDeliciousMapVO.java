54df5518d6b694a966800b2c4cf5be2582388e2a
hwany <rakddh@gmail.com>
2018-09-18 11:25:24 +0900

    20180918 - 신규일 ( MYpage) Class Update

---
+++ Emoji/src/main/java/com/bit/emoji/model/ScrapDeliciousMapVO.java
@@ -0,0 +1,6 @@
+package com.bit.emoji.model;
+
+public class ScrapDeliciousMapVO {
+	private int memberNum;
+	private int deliciousMemberNum;
+}
---
+++ Emoji/src/main/java/com/bit/emoji/mypage/service/MyRouteService.java
@@ -0,0 +1,42 @@
+package com.bit.emoji.mypage.service;
+
+import com.bit.emoji.model.RouteVO;
+
+public class MyRouteService {
+	public RouteVO loadMyRoute(String memberEmail) {
+		return null;
+		
+	}
+	
+	public RouteVO updateMyRoute(int routeNum) {
+		return null;
+	}
+	
+	public RouteVO deleteMyRoute(int routeNum) {
+		return null;
+	}
+	
+	public String stateMyRoute(int routeNum) {
+		return null;
+	}
+	
+	public RouteVO loadScrapRoute(int routeNum) {
+		return null;
+	}
+	
+	public RouteVO deleteScrapRoute(int routeNum) {
+		return null;
+	}
+	
+	public String stateScrapRoute(int routeNum) {
+		return null;
+	}
+	
+	public RouteVO joinRoute(int routeNum) {
+		return null;
+	}
+	
+	public RouteVO deleteJoinRoute(int routeNum) {
+		return null;
+	}
+}
