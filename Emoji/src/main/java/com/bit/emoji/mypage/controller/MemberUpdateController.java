cb614d485b81415465c1ba8ebcae2ddd8c5f1083
hwany <rakddh@gmail.com>
2018-09-20 14:43:33 +0900

    -

--- Emoji/src/main/java/com/bit/emoji/mypage/controller/MemberUpdateController.java
+++ Emoji/src/main/java/com/bit/emoji/mypage/controller/MemberUpdateController.java
@@ -1,17 +1,16 @@
 package com.bit.emoji.mypage.controller;
 
-import com.bit.emoji.model.MemberVO;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.bit.emoji.mypage.service.MemberUpdateService;
 
-public class MemberUpdateController {
-	public MemberUpdateService memberService;
-	public MemberVO selecetMember(int membeNum) {
-		return null;
-	
-	}
-	
-	public MemberVO updateMember(int memberNum) {
-		return null;
-	
-	}
+
 }
